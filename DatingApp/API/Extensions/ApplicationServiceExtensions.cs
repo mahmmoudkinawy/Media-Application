32986bab5d1c9b4b10a501e9cbf99821ec21606c
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-05 01:27:30 +0200

    Updated API & trying to make it architecture

--- DatingApp/API/Extensions/ApplicationServiceExtensions.cs
+++ DatingApp/API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,8 @@ namespace API.Extensions
             IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
---
+++ DatingApp/API/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+using API.DTOs;
+using API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByNameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberAsync(string username);
+    }
+}
