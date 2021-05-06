05e76a1f56ae2b4729ec98fc16df9bad13125a76
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-06 19:31:04 +0200

    Added guards to angulr

---
+++ DatingApp/client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,46 @@
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { User } from 'src/app/_models/user';
+import { Member } from 'src/app/_modules/member';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
+  member: Member;
+  user: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any){
+    if(this.editForm.dirty){
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(private accountService:AccountService,private memberService: MembersService,
+    private toastr: ToastrService) {
+      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    this.memberService.getMember(this.user.username).subscribe( member => {
+      this.member = member;
+    })
+  }
+
+  updateMemeber(){
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm.reset(this.member);
+  }
+
+}
