2cda03606cdbbc7a810e9ec7bcac22b8ca008fdb
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-07 23:22:37 +0200

    Updated bug in interface

--- DatingApp/API/Helpers/UserParams.cs
+++ DatingApp/API/Helpers/UserParams.cs
@@ -15,7 +15,7 @@
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
-        public int MaxAge { get; set; } = 105;
+        public int MaxAge { get; set; } = 100;
 
     }
 }
