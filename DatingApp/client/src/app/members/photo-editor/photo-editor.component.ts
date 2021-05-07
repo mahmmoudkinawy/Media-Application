7d1c2140f48f9116b20c1a1047f69554ab888dc9
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-07 05:35:52 +0200

    Added the delete & main  functionality in angular

--- DatingApp/client/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp/client/src/app/members/photo-editor/photo-editor.component.ts
@@ -43,6 +43,12 @@ export class PhotoEditorComponent implements OnInit {
     })
   }
 
+  deletePhoto(photoId: number){
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter(x => x.id !== photoId);
+    })
+  }
+
   fileOverBase(e: any){
     this.initializeUploader = e;
   }
