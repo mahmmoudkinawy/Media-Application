2bd82954d6996b4ffde9d2b51ace8d6087aa8c40
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-07 21:17:50 +0200

    Added filtering by gender in API

--- DatingApp/API/Data/UserRepository.cs
+++ DatingApp/API/Data/UserRepository.cs
@@ -16,7 +16,7 @@ namespace API.Data
         private readonly DataContext _context;
         private readonly IMapper _mapper;
 
-        public UserRepository(DataContext context,IMapper mapper)
+        public UserRepository(DataContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
@@ -32,11 +32,14 @@ namespace API.Data
 
         public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            var query = _context.Users
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .AsNoTracking();
+            var query = _context.Users.AsQueryable();
 
-            return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+
+            return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper
+                .ConfigurationProvider).AsNoTracking(),
+                    userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
--- DatingApp/API/Helpers/UserParams.cs
+++ DatingApp/API/Helpers/UserParams.cs
@@ -11,5 +11,9 @@
             get => _pageSize;
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
+
+        public string CurrentUsername { get; set; }
+        public string Gender { get; set; }
+
     }
 }
