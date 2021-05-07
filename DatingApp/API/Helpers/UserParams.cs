246438101162b93d63e1b80b8c2fe6a76ded71c0
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-07 21:26:42 +0200

    Modified filtering in the API

--- DatingApp/API/Helpers/UserParams.cs
+++ DatingApp/API/Helpers/UserParams.cs
@@ -14,6 +14,8 @@
 
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 105;
 
     }
 }
