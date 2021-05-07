17a8fd1e0fd8baa3c146fe7c4c0c1471ad570f29
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-07 19:55:37 +0200

    Added Page Pagination in API

--- DatingApp/API/Interfaces/IUserRepository.cs
+++ DatingApp/API/Interfaces/IUserRepository.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace API.Interfaces
         Task<IEnumerable<AppUser>> GetUsersAsync();
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByNameAsync(string username);
-        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
         Task<MemberDto> GetMemberAsync(string username);
     }
 }
