c18d741bfad2916f74d7f8677cc1a30cb77fe695
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-07 23:49:27 +0200

    Added Last Active Featrue in API

--- DatingApp/API/Helpers/UserParams.cs
+++ DatingApp/API/Helpers/UserParams.cs
@@ -15,7 +15,7 @@
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
-        public int MaxAge { get; set; } = 100;
-
+        public int MaxAge { get; set; } = 150;
+        public string OrderBy { get; set; } = "lastActive";
     }
 }
