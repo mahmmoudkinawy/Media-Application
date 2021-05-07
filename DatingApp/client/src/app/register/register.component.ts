ab8b0398910cb2f06dbb6f0c638253e015c5e5c7
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-07 17:03:47 +0200

    Added date for all broswers

--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -12,17 +12,25 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
   registerForm: FormGroup;
+  maxDate: Date;
 
   constructor(private accountService:AccountService, private toastr: ToastrService,
     private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.intitializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   intitializeForm(){
     this.registerForm = this.fb.group({
+      gender: ['male'],
       username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
       password: ['',[Validators.required,
          Validators.minLength(4), Validators.maxLength(8)]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
