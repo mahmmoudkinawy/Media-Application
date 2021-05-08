edc478124c91ebca0519dce02ade0ca55c39ecd5
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-08 19:41:46 +0200

    Added like to the interceptor

--- DatingApp/client/src/app/_interceptors/error.interceptor.ts
+++ DatingApp/client/src/app/_interceptors/error.interceptor.ts
@@ -29,8 +29,10 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
                 throw modalStateErrors.flat();
-              } else {
+              } else if(typeof(error.error) === 'object') {
                 this.toastr.error("Bad Request", error.status);
+              }else {
+                this.toastr.error("You already like this user", error.status);
               }
               break;
             case 401:
