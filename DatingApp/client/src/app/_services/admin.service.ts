2eb095108d3c91bedd35c7ef1a2ead44db63f3c5
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-05-10 03:59:03 +0200

    Added photo & user management client side

--- DatingApp/client/src/app/_services/admin.service.ts
+++ DatingApp/client/src/app/_services/admin.service.ts
@@ -15,7 +15,7 @@ export class AdminService {
     return this.http.get<Partial<User[]>>(this.baseUrl + 'admin/users-with-roles');
   }
 
-  // updateUserRoles(username: string, roles: string[]) {
-  //   return this.http.post(this.baseUrl + 'admin/edit-roles/' + username + '?roles=' + roles, {});
-  // }
+  updateUserRoles(username: string, roles: string[]) {
+    return this.http.post(this.baseUrl + 'admin/edit-roles/' + username + '?roles=' + roles, {});
+  }
 }
--- DatingApp/client/src/app/modals/roles-modal/roles-modal.component.ts
+++ DatingApp/client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-roles-modal',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roles-modal.component.css']
 })
 export class RolesModalComponent implements OnInit {
+  @Input() updateSelectedRoles = new EventEmitter();
+  user: User;
+  roles: any[];
 
-  constructor() { }
+  constructor(public bsModalRef: BsModalRef) { }
 
   ngOnInit(): void {
+  }
+
+  updateRoles() {
+    this.updateSelectedRoles.emit(this.roles);
+    this.bsModalRef.hide();
   }
 
 }
